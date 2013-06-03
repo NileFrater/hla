2a3ff878dc35ad4cb6f0320f5e52c0dd17421006
Noobulater <AmanBentley@gmail.com>
2013-06-03 17:35:20 -0400

    Update breensreserve.lua

--- gamemode/schemas/hl2rp/items/breensreserve.lua
+++ gamemode/schemas/hl2rp/items/breensreserve.lua
@@ -5,19 +5,16 @@ ITEM.Model = "models/props_junk/popcan01a.mdl"
 ITEM.Purchaseable = true
 ITEM.Price = 4
 ITEM.ItemGroup = 1
-function ITEM:Drop(ply)
+ITEM.Hunger = 4
 
-end
+ function ITEM:Drop(ply)
+ 
+ end
 
-function ITEM:Pickup(ply)
-
-	self:Remove()
-
-end
-
-function ITEM:UseItem(ply)
-
-	ply:TakeItem("breensreserve")	
-	self:Remove()
-
-end
+ function ITEM:UseItem(ply)
+ 
+   ply:TakeItem("breensreserve")
+   ply:SetHunger(ply:GetHunger() + self.Hunger)  
+   self:Remove()
+ 
+ end
